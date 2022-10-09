f811d74a45b3e03772eb4d8b421f03065b878021
zhiyun00 <yuanbaoye2000@163.com>
2022-10-09 22:02:43 +0800

    fourth commit

---
+++ view_user/src/components/right/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about" style="line-height: 26px; font-size: 15px; color: #888">
+    <span>WHU论坛</span><br />
+    <span>日期：2022.10.</span><br />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
